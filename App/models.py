77b25722925c8262df3191e4f2327ff237bb7e4e
AbrahamupSky <abrahamup7@gmail.com>
2024-03-14 13:30:22 -0600

    arreglar los modelos y continuar

--- App/models.py
+++ App/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+class Record(models.Model):
+  PRODUCTS = (
+    ('Large Fruit', 'Large Fruit'),
+    ('Medium Fruit', 'Medium Fruit'),
+    ('Small Fruit', 'Small Fruit'),
+    ('Parfait', 'Parfait'),
+    ('Grilled Wrap', 'Grilled Wrap'),
+    ('Spicy Wrap', 'Spicy Wrap'),
+    ('Kale Salad', 'Kale Salad'),
+    ('Market Salad', 'Market Salad'),
+    ('Southwest Salad', 'Southwest Salad'),
+    ('Cobb Salad', 'Cobb Salad'),
+    ('Side Salad', 'Side Salad'),
+  )
+
+  QUANTITYS = (
+    ('1', '1'),
+    ('2', '2'),
+    ('3', '3'),
+    ('4', '4'),
+    ('5', '5'),
+    ('6', '6'),
+    ('7', '7'),
+    ('8', '8'),
+    ('9', '9'),
+    ('10', '10'),
+    ('11', '11'),
+    ('12', '12'),
+    ('13', '13'),
+    ('14', '14'),
+    ('15', '15'),
+    ('16', '16'),
+    ('17', '17'),
+    ('18', '18'),
+    ('19', '19'),
+    ('20', '20'),
+  )
+  
+  product = models.CharField(max_length=100, choices=PRODUCTS)
+  quantity = models.CharField(max_length=100, choices=QUANTITYS)
+  done = models.BooleanField(default=False)
+  date = models.DateField()
+  # creation_date = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return f"{self.product} - {self.date}"
+  
+class Production(models.Model):
+  product = models.ForeignKey(Record, on_delete=models.CASCADE)
+  quantity = models.CharField(max_length=100)
+  done = models.BooleanField(default=False)
+  date = models.DateField()
+  # creation_date = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return f"{self.product} - {self.date}"
--- App/views.py
+++ App/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import auth
 from django.contrib.auth import authenticate
 from .forms import CreateUserForm, LoginForm
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # ? - Home page
 def home(request):
@@ -16,7 +17,7 @@ def register(request):
 
     if form.is_valid():
       form.save()
-      # return redirect('')
+      return redirect('login')
 
   context = {'form': form}
 
@@ -36,8 +37,18 @@ def login(request):
 
       if user is not None:
         auth.login(request, user)
-        # return redirect('')
+        return redirect('dashboard')
 
   context = {'form': form}
 
   return render(request, 'App/login.html', context=context)
+
+# ? - Logout user
+def logout(request):
+  auth.logout(request)
+  return redirect('login')
+
+# ? Dashboard
+@login_required(login_url='login')
+def dashboard(request):
+  return render(request, 'App/dashboard.html')
